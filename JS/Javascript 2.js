0043e135eeb3f616547f19fbb8ccc3b9f98df763
Osei Alex King Junior <dextroalex@gmail.com>
2017-12-24 11:38:44 +0000

    changes 24/12/2017

---
+++ JS/Javascript 2.js
@@ -0,0 +1,16 @@
+<script language="javascript">
+            function(form)
+            {
+                if (form.userid.value == "Alex" && form.pwd.value == "Lynx2016")
+                {
+                    return true;
+                }
+                else
+                {
+                    alert("Error Password or Username")
+                    return false;
+                }    
+            
+            }
+            </script>
+alert("welcome")
